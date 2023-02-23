59cc54d3323619ea58ba6d63f17dd94ca31d6b58
jakubKuznik <kuznja16@gmail.com>
2023-02-23 19:16:55 +0100

    init

---
+++ attacks/DDOS/dos.c
@@ -0,0 +1,14 @@
+// Solution for BACHELOR’S THESIS: atack generation on industrial modbus network 
+// File:        dos.c
+// Author:      Jakub Kuzník, FIT
+//  
+
+// Execution:
+
+#include "dos.h"
+
+
+
+int main(int argc, char *argv[]) {
+    printf("hi");
+}
---
+++ attacks/DDOS/dos.h
@@ -0,0 +1,31 @@
+// Solution for BACHELOR’S THESIS: atack generation on industrial modbus network 
+// File:        dos.h
+// Author:      Jakub Kuzník, FIT
+//  
+
+#ifndef FLOW_H 
+#define FLOW_H
+
+
+// normal libraries 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <sys/time.h>
+#include <time.h>
+#include <signal.h>
+
+// network libraries
+#include <arpa/inet.h>
+#include <netinet/if_ether.h> //ethernet and arp frame 
+#include <netinet/ip_icmp.h>
+#include <netinet/ip6.h>
+#include <netdb.h>
+#include <pcap/pcap.h>
+
+
+
+
+#endif 
